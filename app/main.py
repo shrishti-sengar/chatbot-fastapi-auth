c4315ca7b6ec5e12cad2da05e73358331cd9dc23
shrishti <shrishti2994@gmail.com>
2025-10-18 13:19:17 +0530

    BASE WORKING CORRECT

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
-load_dotenv()  # loads .env into environment
+load_dotenv()
 from fastapi import FastAPI
 from app.routers import chat, auth, history, admin, dev
-from app import models, database
+from app.database import Base, engine
 
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Chatbot API with Auth")
 
 # include routers
@@ -13,17 +14,4 @@ app.include_router(auth.router)
 app.include_router(history.router)
 app.include_router(admin.router)
 app.include_router(dev.router)
-
-
-@app.get("/hello")
-def hello():
-    return {"message": "Hello Shrishti 👋, FastAPI is running!"}
-
-@app.get("/add")
-def add(a: int, b: int):
-    return {"a": a, "b": b, "sum": a + b}
-
-@app.get("/items/{item_id}")
-def get_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "query": q}
 
