c60d0360afab9e83860c57e9bf4af39ca960ebac
shrishti <shrishti2994@gmail.com>
2025-10-18 13:21:05 +0530

    4 files deleted

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Running DB init..."
-python -m app.init_db
-
-# exec the container CMD (this replaces the shell process)
-exec "$@"
--- test.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = "https://public-api.wordpress.com/wpcom/v2/work-with-us"
-response = requests.get(url)
-print(response.json())
